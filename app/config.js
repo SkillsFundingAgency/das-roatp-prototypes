8c0b420f3a0aeb163bc99ca85a4b101b7fe39e26
xedhead <xedhead@gmail.com>
2019-01-11 11:20:49 +0000

    declarations - wip

--- app/config.js
+++ app/config.js
@@ -4,7 +4,7 @@
 
 module.exports = {
   // Service name used in header. Eg: 'Renew your passport'
-  serviceName: 'Register as a training provider',
+  serviceName: 'Apply to the register of apprenticeship training providers',
 
   // Default port that prototype runs on
   port: '3000',

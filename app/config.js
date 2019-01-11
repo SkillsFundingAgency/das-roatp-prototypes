db376bc36514abba63d41c6fa77dd7c79fa08057
xedhead <xedhead@gmail.com>
2019-01-11 11:38:28 +0000

    changed service name

--- app/config.js
+++ app/config.js
@@ -4,7 +4,7 @@
 
 module.exports = {
   // Service name used in header. Eg: 'Renew your passport'
-  serviceName: 'Apply to the register of apprenticeship training providers',
+  serviceName: 'Apply to become apprenticeship training provider',
 
   // Default port that prototype runs on
   port: '3000',

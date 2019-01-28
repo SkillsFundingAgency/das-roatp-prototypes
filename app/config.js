01b8da0b48a5507d8f4519122671ef15c55d44bf
xedhead <xedhead@gmail.com>
2019-01-28 11:50:21 +0000

    tweaks from morning sess

--- app/config.js
+++ app/config.js
@@ -4,7 +4,7 @@
 
 module.exports = {
   // Service name used in header. Eg: 'Renew your passport'
-  serviceName: 'Apply to become apprenticeship training provider',
+  serviceName: 'Manage the RoATP',
 
   // Default port that prototype runs on
   port: '3000',

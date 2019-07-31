62c3939001a570caf7fbcb7a49ab9859a6948a00
ESFA <esfa@Westleys-13-MacBook-Pro.local>
2019-07-31 15:43:56 +0100

    Small update to task list content. Creation of staff app v5. Addition of dashboard following sing in to staff app.

--- app/routes.js
+++ app/routes.js
@@ -20,6 +20,7 @@ require('./routes/staff-app/v2.js')(router);
 require('./routes/staff-app/v2a.js')(router);
 require('./routes/staff-app/v3.js')(router);
 require('./routes/staff-app/v4.js')(router);
+require('./routes/staff-app/v5.js')(router);
 
 // Eligibility Checker
 require('./routes/eligibility/bravo.js')(router); // Bravo

8d4701c358830c2d312486ecb23ed15b6cae7e38
ESFA <esfa@Westleys-13-MacBook-Pro.local>
2019-08-30 13:50:33 +0100

    Set up of v7 for application journey. All textareas set to 2000 characters.

--- app/routes.js
+++ app/routes.js
@@ -11,6 +11,7 @@ require('./routes/application/v3.js')(router);
 require('./routes/application/v4.js')(router);
 require('./routes/application/v5.js')(router);
 require('./routes/application/v6.js')(router);
+require('./routes/application/v7.js')(router);
 
 // Organisation Journey
 require('./routes/organisation/v01.js')(router);

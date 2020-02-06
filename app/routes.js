5782c2663ec5a8c8687132eb064282f7f4264d2e
ESFA <esfa@Westleys-13-MacBook-Pro.local>
2020-02-06 15:59:06 +0000

    Setup of v2 for gateway and assessor. APR-1090 done.

--- app/routes.js
+++ app/routes.js
@@ -26,9 +26,11 @@ require('./routes/organisation/v0.js')(router);
 
 // MVP Gateway
 require('./routes/mvp-gateway/v1.js')(router);
+require('./routes/mvp-gateway/v2.js')(router);
 
 // MVP Assessor
 require('./routes/mvp-assessor/v1.js')(router);
+require('./routes/mvp-assessor/v2.js')(router);
 
 // Staff App
 require('./routes/staff-app/v2.js')(router);

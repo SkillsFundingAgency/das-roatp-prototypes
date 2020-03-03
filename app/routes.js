8f4e68bf2145459f4d588162146f92ee7dda8742
ESFA <esfa@Westleys-13-MacBook-Pro.local>
2020-03-03 15:44:59 +0000

    Set up of MVS Gateway v5

--- app/routes.js
+++ app/routes.js
@@ -35,6 +35,7 @@ require('./routes/mvp-gateway/v1.js')(router);
 require('./routes/mvp-gateway/v2.js')(router);
 require('./routes/mvp-gateway/v3.js')(router);
 require('./routes/mvp-gateway/v4.js')(router);
+require('./routes/mvp-gateway/v5.js')(router);
 
 // MVP Assessor
 require('./routes/mvp-assessor/v1.js')(router);
